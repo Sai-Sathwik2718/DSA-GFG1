2e962ee8c20991a9f9e7756b2735e8a3e9fd3e13
Sai-Sathwik2718 <158552902+Sai-Sathwik2718@users.noreply.github.com>
2025-12-08 17:46:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Arithmetic Number/arithmetic-number.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+
+class Solution {
+    static int inSequence(int a, int b, int c) {
+        // code here
+        if((b-a)%c==0) return 1;
+        else return 0;
+    }
+}
