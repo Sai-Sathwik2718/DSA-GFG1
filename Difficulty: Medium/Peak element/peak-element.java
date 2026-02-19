506cb8de06af804594b8bb5109b35266876b6a05
Sai-Sathwik2718 <158552902+Sai-Sathwik2718@users.noreply.github.com>
2026-02-19 23:50:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Peak element/peak-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int peakElement(int[] arr) {
+        // code here
+        int n=arr.length;
+        int low=0,high=n-1;
+        while(low<high){
+            int mid=(low+high)/2;
+            if(arr[mid]>arr[mid+1]){
+                high=mid;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+    
+}
